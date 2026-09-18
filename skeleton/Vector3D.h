6d3ca054ee064310d412616885b2d0f4ca89a855
Oliver García <122832090+MaltenGD@users.noreply.github.com>
2026-09-18 12:50:24 +0200

    Nuevos operadores para Vector3D

--- skeleton/Vector3D.h
+++ skeleton/Vector3D.h
@@ -14,6 +14,9 @@ public:
 	Vector3D cross(const Vector3D& v) const 
 	{ return {(y*v.z)-(z * v.y), (z * v.x) - (x * v.z) , (x * v.y) - (y * v.x) }; };
 	Vector3D operator+(const Vector3D& v) const { return { x + v.x, y + v.y, z + v.z }; }
+	Vector3D operator-(const Vector3D& v) const { return { x - v.x, y - v.y, z - v.z }; }
+	Vector3D operator*(const float scalar) const { return { x * scalar, y * scalar, z * scalar }; }
+	Vector3D opearator(const Vector3D & v) { x += v.x; y += v.y; z += v.z; return *this; }
 	operator physx::PxVec3() const
 	{
 		return physx::PxVec3(x, y, z);
